cb51f0f161bbd1070dd94097e441c440fd1632fc
U-Lenovo\Tammie <tammie.kahnhauser@gmail.com>
2016-05-12 18:15:14 -0700

    question on doctests, ask TA first thing tomorrow

--- functions.py
+++ functions.py
@@ -27,44 +27,53 @@ def compute_jaccard_index(s1, s2):
     # question mark goes first so it doesn't delete numbers (don't know why)
     # " and - must be escaped.
     set_1 = set(re.split("[?\s.,\-!\"]", s1.lower()))
-    # print set_1
     set_2 = set(re.split("[?\s.,\-!\"]", s2.lower()))
-    # print set_2
+
+    # remove empty strings artifacts from punctuation
     set_1.discard('')
-    # set_1.discard('and')
     set_2.discard('')
-    # set_2.discard('and')
+
+    # compute worsd in common between two strings
     common_words = len(set_1.intersection(set_2))
-    # print common_words
+    # compute total individual words across both strings
     all_words = float(len(set_1) + len(set_2) - common_words)
-    # print all_words
+
     return common_words / all_words
 
 
-def make_edge_list():
-    edges = {}
-    # print "Creating Edge List..."
-    all_lines = Line.query.all()
+def make_edge_list(str_list):
+    """
+    How to make doctests if the function input is database-dependent?
+    specifically, input is Line object.
 
-    for i in range(len(all_lines)):
-        line1 = all_lines[i]
-        # print line1.line_no
-        for line2 in all_lines[i+1:]:
+    """
+
+    # initiate empty dictionary
+    edges = {}
+
+    for i in range(len(str_list)):
+        line1 = str_list[i]
+        # check only those lines after the one you're looking at
+        # (to prevent duplicate matches)
+        for line2 in str_list[i+1:]:
+            # check jaccard similarity
             if compute_jaccard_index(line1.lyrics, line2.lyrics) > .34:
+                # add similar lines to the adjacency list
                 edges[line1] = edges.get(line1, [])
                 edges[line1].append(line2)
     return edges
 
 
-# if __name__ == "__main__":
-#     """Create Adjacency List"""
+if __name__ == "__main__":
+    """Create Adjacency List"""
 
-#     from server import app
-#     connect_to_db(app)
-#     print "Connected"
+    from server import app
+    connect_to_db(app)
+    print "Connected"
+    create master list of all lines in database
+    all_lines = Line.query.all()
+    # Print the edge list (ideally to a txt file)
+    pprint.pprint(make_edge_list(all_lines))
 
-#     # Print the edge list (ideally to a txt file)
-#     pprint.pprint(make_edge_list())
-
-# else:
-#     print "Never ran."
+else:
+    print "Never ran."
