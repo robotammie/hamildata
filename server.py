2a582953bff3fd11286566f387ebcaff42f081ea
U-Lenovo\Tammie <tammie.kahnhauser@gmail.com>
2016-05-20 11:57:21 -0700

    added test json data to the output of compare_songs.json route

--- server.py
+++ server.py
@@ -82,7 +82,10 @@ def compare_songs():
 
     # TODO: rewrite comp_lines so that it returns a dictionary of tuples, as per notebook sketch
 
-    return jsonify('totally a dictionary')
+    return jsonify({('char 1, song 1', 'line 1, song 1'): [('char 1, song 2', 'line 1, song 2')],
+                    ('char 2, song 1', 'line 2, song 1'): [('char 2, song 2', 'line 2, song 2'),
+                                                           ('char 3, song 2', 'line 3, song 2')]
+                    })
 
 
 ##########################################################
